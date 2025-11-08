08a356bbc2f301bfca9bfae58462c7a3d5b42d06
Cajul <creapxinnovacion@gmail.com>
2025-11-08 00:40:53 -0600

    Version 1.3

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "🚀 Iniciando servidor FastAPI..."
+echo "🚀 Iniciando servidor FastAPI en Render..."
 
 APP_PATH="main:app"
 HOST="0.0.0.0"
 PORT=${PORT:-8000}
 
-# Ejecutar uvicorn desde Python
+# Ejecutar uvicorn usando python -m
 exec python -m uvicorn $APP_PATH --host $HOST --port $PORT
